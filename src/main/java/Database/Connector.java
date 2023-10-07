8ae1578ff233fb176ee845e68a20729de8cf8d38
vinhquangnguyen231203 <vinhquangnguyennn@gmail.com>
2023-10-07 16:45:11 +0700

    Push

---
+++ src/main/java/Database/Connector.java
@@ -0,0 +1,46 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connector {
+
+    public static Connection getMySQLConnection() {
+        String hostName = "localhost";
+        String dbName = "product-db-pm05";
+        String userName = "root";
+        String password = "231203";
+
+        return getMySQLConnection(hostName, dbName, userName, password);
+
+    }
+
+    private static Connection getMySQLConnection(String hostName, String dbName, String userName, String password) {
+
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(connectionURL, userName, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+
+    }
+
+    public static void closeConnection(Connection conn){
+        try {
+            if(conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}

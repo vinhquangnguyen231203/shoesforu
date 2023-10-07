1ad4cfc50c19475b276369dd2d79a7e89486e31d
vinhquangnguyen231203 <vinhquangnguyennn@gmail.com>
2023-10-07 21:23:13 +0700

    Quang

---
+++ src/main/java/Controller/HomeController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet("home_view")
+public class HomeController extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/WEB-INF/JSP/View/Home_View.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+    
+}
